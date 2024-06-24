b6a0f4ea111b01f52ba4931010a38a6f231df283
Prabhakar Naik Kimavath <prabhakarnauik225@gmail.com>
2024-06-24 16:14:11 +0530

    project

---
+++ src/main/java/com/springboot/core/EvenNumbers.java
@@ -0,0 +1,17 @@
+package com.springboot.core;
+
+/**
+ * @author prabhakar, @Date 24-06-2024
+ */
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+
+        int number = 25;
+
+        if (number % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Not Even");
+    }
+}
---
+++ src/main/java/com/springboot/oops/abstraction/MyClass.java
@@ -0,0 +1,8 @@
+package com.springboot.oops.abstraction;
+
+/**
+ * @author prabhakar, @Date 24-06-2024
+ */
+public class MyClass {
+    // Encapsulated
+}
