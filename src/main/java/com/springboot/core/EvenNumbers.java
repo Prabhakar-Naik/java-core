cb9fb3eeb42922c5cfd1197d053ec1c7a45d2414
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-06-24 17:16:05 +0530

    straems added

--- src/main/java/com/springboot/core/EvenNumbers.java
+++ src/main/java/com/springboot/core/EvenNumbers.java
@@ -1,6 +1,7 @@
 package com.springboot.core;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * @author prabhakar, @Date 24-06-2024
@@ -15,6 +16,13 @@ public class EvenNumbers {
             System.out.println("Even");
         else
             System.out.println("Not Even");
+
+
+        System.out.println("Using for loop");
+        for (int i =1; i<=10; i++){
+            if (i % 2 == 0)
+                System.out.println(i);
+        }
 
 
     }
