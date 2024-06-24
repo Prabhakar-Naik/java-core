bdd96ee32fd36292151a5c0d07ffb915f66f39c2
Prabhakar Naik <developerspirit95@gmail.com>
2024-06-24 17:20:52 +0530

    Create OddNumbers.java

---
+++ src/main/java/com/springboot/core/OddNumbers.java
@@ -0,0 +1,23 @@
+
+
+public class OddNumbers {
+
+    public static void main(String[] args) {
+
+        int number = 25;
+
+        if (number % 2 != 0)
+            System.out.println("Odd");
+        else
+            System.out.println("Not Odd");
+
+
+        System.out.println("Using for loop");
+        for (int i =1; i<=10; i++){
+            if (i % 2 != 0)
+                System.out.println(i);
+        }
+
+
+    }
+}
