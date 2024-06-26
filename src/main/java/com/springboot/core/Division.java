84748528ffaaea343998fc5a0ccdd1526d0536f8
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-06-26 15:06:56 +0530

    custome exception

---
+++ src/main/java/com/springboot/core/Division.java
@@ -0,0 +1,22 @@
+package com.springboot.core;
+
+import com.springboot.exceptions.LargeNumDivisionLowerNumException;
+
+/**
+ * @author prabhakar, @Date 26-06-2024
+ */
+public class Division {
+
+    public static void main(String[] args) {
+
+        // this is the sample for custom exception the java given result is right
+
+        int x =30;
+        int y = 20;
+        if (x > y)
+            System.out.println(x%y);
+        else
+            throw new LargeNumDivisionLowerNumException();
+
+    }
+}
---
+++ src/main/java/com/springboot/exceptions/LargeNumDivisionLowerNumException.java
@@ -0,0 +1,12 @@
+package com.springboot.exceptions;
+
+/**
+ * @author prabhakar, @Date 26-06-2024
+ */
+public class LargeNumDivisionLowerNumException extends RuntimeException {
+
+    public LargeNumDivisionLowerNumException() {
+        super("Division by larger number is not allowed for modulo operation.");
+    }
+
+}
