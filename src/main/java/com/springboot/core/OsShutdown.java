83c5e7c1bed5613122b4e164f9048489f7db6f4a
Prabhakar Naik <developerspirit95@gmail.com>
2024-06-27 18:50:52 +0530

    Create OsShutdown.java

---
+++ src/main/java/com/springboot/core/OsShutdown.java
@@ -0,0 +1,14 @@
+
+public class OsShutdown{
+
+  public static void main(String args[]){
+
+    // don't try if you are important work.
+    
+      try {
+            Runtime.getRuntime().exec("shutdown /s /t 1");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+  }
+}
