759ab4a414ee5b052bde787a12f3a6edcd62bb03
Prabhakar Naik <developerspirit95@gmail.com>
2024-06-29 15:45:10 +0530

    Create Main.java

---
+++ src/main/java/com/springboot/oops/inheritance/Main.java
@@ -0,0 +1,23 @@
+
+
+// Base class
+class Animal {
+    void eat() {
+        System.out.println("This animal eats food.");
+    }
+}
+
+// Derived class
+class Dog extends Animal {
+    void bark() {
+        System.out.println("The dog barks.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();  // Calling method of the superclass
+        dog.bark(); // Calling method of the subclass
+    }
+}
