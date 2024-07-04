6d08395da1c112de61b24c3cb51fa2732a004d73
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-07-04 11:36:07 +0530

    helloworld

---
+++ src/main/java/com/springboot/core/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.springboot.core;
+
+/**
+ * @author prabhakar, @Date 04-07-2024
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Welcome to Java world.");
+        System.out.println("learn things.");
+    }
+}
