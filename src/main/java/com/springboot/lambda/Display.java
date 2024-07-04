add0ec828000382b4aed90579aa687a1a8571db3
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-07-04 11:48:41 +0530

    lambda functions

---
+++ src/main/java/com/springboot/lambda/Display.java
@@ -0,0 +1,9 @@
+package com.springboot.lambda;
+
+
+@FunctionalInterface
+public interface Display {
+
+	void display(String msg);	// functionalInterface have only one abstract method not more than one
+	
+}
---
+++ src/main/java/com/springboot/lambda/LambdaApp.java
@@ -0,0 +1,46 @@
+package com.springboot.lambda;
+
+@FunctionalInterface
+ interface  Cab {
+	void bookUber();
+}
+
+class bookCab implements Cab{
+
+	@Override
+	public void bookUber() {
+		System.out.println("This is bookUber method in bookCab class ... printing soon!");
+		
+	}
+	
+}
+public class LambdaApp {
+
+	public static void main(String[] args) {
+		//1.
+//		Cab cab=new bookCab();
+//		cab.bookUber();
+		
+		//2.
+		//Anonymous class implementation
+//		Cab cab=new Cab() {
+//
+//			@Override
+//			public void bookUber() {
+//				System.out.println("this is anonymous method implementation...");
+//				
+//			}
+//		};
+//		cab.bookUber();
+		
+		//3.
+		//using Lambda
+		
+		Cab cab= ()->{
+			System.out.println("this is lambda Expression using functional Interface easy code...");
+		};
+		
+		cab.bookUber();
+		
+	}
+}
