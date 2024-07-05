10fd7f8abae1c54112229afa83a4bf731468ef1f
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-07-05 14:58:59 +0530

    basic programs

---
+++ src/main/java/com/springboot/core/basic/RelationalOper.java
@@ -0,0 +1,25 @@
+package com.springboot.core.basic;
+
+public class RelationalOper {
+
+	public static void main(String[] args) {
+		
+		int i=20,j=6;
+		System.out.println("Original Order i to j: ");
+		System.out.println(i==j);
+		System.out.println(i<=j);
+		System.out.println(i>=j);
+		System.out.println(i!=j);
+		System.out.println(i>j);
+		System.out.println(i<j);
+		
+		System.out.println("Reverse Order j to i: ");
+		System.out.println(j==i);
+		System.out.println(j<=i);
+		System.out.println(j>=i);
+		System.out.println(j!=i);
+		System.out.println(j>i);
+		System.out.println(j<i);
+		
+	}
+}
