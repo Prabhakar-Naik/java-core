5da5ef83bf885f50ee49f919ca9ea6ae876d47bd
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-07-22 15:31:07 +0530

    logic

--- src/main/java/com/springboot/core/armstrong/ArmstrongNumber.java
+++ src/main/java/com/springboot/core/armstrong/ArmstrongNumber.java
@@ -5,21 +5,29 @@ import java.util.Scanner;
 public class ArmstrongNumber {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Enter a number: ");
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int d,rev=0,temp=n;
 
-		while(n>0)
-		{
-			d=n%10;
-			rev=rev+(d*d*d); //Math.pow(d, 3);
-			n=n/10;
+		System.out.println("Enter a number: ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int d, rev = 0, temp = n;
+		if (temp >= 1 && temp <= 9) {
+			System.out.println(temp + " Armstrong number");
+			return;
+		} else if (temp >= 10 && temp <= 100) {
+			System.out.println(n+" Not an Armstrong number");
+		}else {
+
+			while (n > 0) {
+				d = n % 10;
+				rev = rev + (d * d * d); //Math.pow(d, 3);
+				n = n / 10;
+			}
+			if (temp == rev)
+				System.out.println(temp + " is Armstrong Number");
+			else
+				System.out.println(temp+" Not an Armstrong number");
+
+			sc.close();
 		}
-		if(temp==rev)
-			System.out.println(temp+ " is Armstrong Number");
-		
-		sc.close();
 	}
 }
--- src/main/java/com/springboot/core/armstrong/ArmstrongNumbers.java
+++ src/main/java/com/springboot/core/armstrong/ArmstrongNumbers.java
@@ -4,31 +4,28 @@ import java.util.Scanner;
 
 public class ArmstrongNumbers {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Enter a Armstrong number Range: ");
-		Scanner sc=new Scanner(System.in);
-		int range=sc.nextInt();
-		
-		 int n, count = 0, a, b, c, sum = 0;
-	        System.out.println("Armstrong numbers from 1 to "+range);
-	        for(int i = 1; i <= range; i++)
-	        {
-	        	
-	            n = i;
-	            while(n > 0)
-	            {
-	                b = n % 10;
-	                sum = sum + (b * b * b);
-	                n = n / 10;
-	            }
-	            if(sum == i)
-	            {
-	                System.out.print(i+" ");
-	            }
-	            sum = 0;
-	        }
-		
-		sc.close();
-	}
+    public static void main(String[] args) {
+
+        System.out.println("Enter a Armstrong number Range: ");
+        Scanner sc = new Scanner(System.in);
+        int range = sc.nextInt();
+
+        int n, count = 0, b, sum = 0;
+        System.out.println("Armstrong numbers from 1 to " + range);
+        for (int i = 100; i <= range; i++) {
+            n = i;
+
+            while (n > 0) {
+                b = n % 10;
+                sum = sum + (b * b * b);
+                n = n / 10;
+            }
+            if (sum == i) {
+                System.out.print(i + " ");
+            }
+            sum = 0;
+        }
+
+        sc.close();
+    }
 }
