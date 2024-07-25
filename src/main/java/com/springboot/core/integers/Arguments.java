43732e26089aba8f70e682a53ca5be0fe9691d6c
DeveloperSpiritTeam-95 <developerspirit95@gmail.com>
2024-07-25 20:57:54 +0530

    sample

---
+++ src/main/java/com/springboot/core/integers/Arguments.java
@@ -0,0 +1,14 @@
+package com.springboot.core.integers;
+
+/**
+ * @author prabhakar, @Date 25-07-2024
+ */
+public class Arguments {
+    public static void main(String[] args) {
+        for (String t: args) {
+            System.out.println(t);
+        }
+
+        System.out.println("Hi Arguments");
+    }
+}
