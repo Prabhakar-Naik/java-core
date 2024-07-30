235f6a4056fd910c03e373ee0b8827d01b981c50
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-07-30 14:28:07 +0530

    changes

--- src/main/java/com/springboot/core/armstrong/ArmstrongNumber.java
+++ src/main/java/com/springboot/core/armstrong/ArmstrongNumber.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 
 public class ArmstrongNumber {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println("Enter a number: ");
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int d, rev = 0, temp = n;
-		if (temp >= 1 && temp <= 9) {
-			System.out.println(temp + " Armstrong number");
-			return;
-		} else if (temp >= 10 && temp <= 100) {
-			System.out.println(n+" Not an Armstrong number");
-		}else {
+        System.out.println("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int digit, reverse = 0, temp = number;
+        if (temp >= 1 && temp <= 9) {
+            System.out.println(temp + " Armstrong number");
+            return;
+        } else if (temp >= 10 && temp <= 100) {
+            System.out.println(number + " Not an Armstrong number");
+        } else {
 
-			while (n > 0) {
-				d = n % 10;
-				rev = rev + (d * d * d); //Math.pow(d, 3);
-				n = n / 10;
-			}
-			if (temp == rev)
-				System.out.println(temp + " is Armstrong Number");
-			else
-				System.out.println(temp+" Not an Armstrong number");
+            while (number > 0) {
+                digit = number % 10;
+                reverse = reverse + (digit * digit * digit); //Math.pow(d, 3);
+                number = number / 10;
+            }
+            if (temp == reverse)
+                System.out.println(temp + " is Armstrong Number");
+            else
+                System.out.println(temp + " Not an Armstrong number");
 
-			sc.close();
-		}
-	}
+            sc.close();
+        }
+    }
 }
