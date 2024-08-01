1b06fa2f07e30ab52f2e80edef564a4b05838c10
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-08-01 20:29:51 +0530

    static blocks

--- src/main/java/com/springboot/methods/StaticBlock.java
+++ src/main/java/com/springboot/methods/StaticBlock.java
@@ -1,28 +1,28 @@
-//package com.springboot.methods;
-//
-///**
-// * @author prabhakar, @Date 01-08-2024
-// */
-//public class StaticBlock {
-//
-//    public static void main(String[] args) {
-//        System.out.println("Main method is executed.");
-//    }
-//
-//    static {
-//        System.out.println("Static block is executed before main method.");
-//    }
-//}
-////Static Block Application …. We need to open Program in specific window
-//class StaticBlock1 {
-//    public static void main(String[] args) {
-//        System.out.println("You are using Windows_NT operating system.");
-//    }
-//
-//    static {
-//        String os = System.getenv("OS");
-//        if (os.equals("Windows") != true) {
-//            System.exit(1);
-//        }
-//    }
-//}
+package com.springboot.methods;
+
+/**
+ * @author prabhakar, @Date 01-08-2024
+ */
+public class StaticBlock {
+
+    public static void main(String[] args) {
+        System.out.println("Main method is executed.");
+    }
+
+    static {
+        System.out.println("Static block is executed before main method.");
+    }
+}
+//Static Block Application …. We need to open Program in specific window
+class StaticBlock1 {
+    public static void main(String[] args) {
+        System.out.println("You are using Windows_NT operating system.");
+    }
+
+    static {
+        String os = System.getenv("OS");
+        if (os.equals("Windows") != true) {
+            System.exit(1);
+        }
+    }
+}
