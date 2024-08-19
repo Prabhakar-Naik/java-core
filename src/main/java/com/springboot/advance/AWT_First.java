383367220ebe76a84d660ca905106d23481a6f2b
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-08-19 14:21:14 +0530

    awt programs

---
+++ src/main/java/com/springboot/advance/AWT_First.java
@@ -0,0 +1,21 @@
+package com.springboot.advance;
+
+import java.awt.*;
+
+/**
+ * @author prabhakar, @Date 19-08-2024
+ */
+public class AWT_First extends Frame {
+    AWT_First() {
+        Button b = new Button("click me");
+        b.setBounds(30, 100, 80, 30);// setting button position
+        add(b);//adding button into frame
+        setSize(300, 300);//frame size 300 width and 300 height
+        setLayout(null);//no layout manager
+        setVisible(true);//now frame will be visible, by default not visible
+    }
+
+    public static void main(String[] args) {
+        AWT_First f = new AWT_First();
+    }
+}
