e52a8c8f4631ad752789fb4ef831dbbd86c5f5fb
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-08-27 08:45:25 +0530

    cast programs

---
+++ src/main/java/com/springboot/core/typecast/StringToLongExample.java
@@ -0,0 +1,13 @@
+package com.springboot.core.typecast;
+
+/**
+ * @author prabhakar, @Date 23-08-2024
+ */
+public class StringToLongExample {
+
+    public static void main(String args[]){
+        String s="9990449935";
+        long l=Long.parseLong(s);
+        System.out.println(l);
+    }
+}
